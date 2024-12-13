901c31a4392b04aead5017f1587433e95dbf5f57
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-13 19:52:03 -0300

    Aplicando while/else

---
+++ atividade_70.py
@@ -0,0 +1,9 @@
+frase  = 'O Phyton é uma linguagem de programação multiparadigma. Phyton foi criado por Guido vam Rossum'
+
+i = 0
+while i < len(frase):
+    letra_atual = frase[i]
+    print(letra_atual.count)
+    i += 1
+else:
+    print("Entrou no else")
