e3e8a0aca5128179a80a71d07af0479a6272d8f4
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-13 20:54:07 -0300

    Reajustes na atividade
    Criado: Um programa que verifica a quantidade de vezes que um caracter aparece em uma string
    
    Conteudo usado: While & else , recurso py.upper(), e if

--- atividade_70.py
+++ atividade_70.py
@@ -1,9 +1,27 @@
-frase  = 'O Phyton é uma linguagem de programação multiparadigma. Phyton foi criado por Guido vam Rossum'
-
+frase  = 'O Phyton é uma linguagem de programação multiparadigma. Phyton foi criado por Guido vam Rossum'.upper()
+# .upper() -> Deixa todo o texto maiusculo
 i = 0
+letra_q_apreceu_mais = ""
+qtd_q_mais_aparece = 0
 while i < len(frase):
-    letra_atual = frase[i]
-    print(letra_atual.count)
+    """ 
+    # Não permite a caputura de espaços " "
+    
+    if letra_atual == " ":
+        i += 1
+        continue
+        
+    """
+    if frase[i] != " ": # Não permite a caputura de espaços " "
+        letra_atual = frase[i] # Adiciona a letra diacordo o contador 
+        
+        quantas_veses_ela_aparece = frase.count(letra_atual) # .count() é utilizado para conta quantas vezes uma letra, palavra, numero ou frase etc... aparece em um texto/palavra em uma string.
+        
+        if qtd_q_mais_aparece < quantas_veses_ela_aparece:
+            print(letra_atual)
+            qtd_q_mais_aparece = quantas_veses_ela_aparece
+            letra_q_apreceu_mais = letra_atual
+        print(letra_atual," |> ",quantas_veses_ela_aparece)
     i += 1
 else:
-    print("Entrou no else")
+    print(f"A letra com a maior quantidade de vezes que apreceu foi a '{letra_q_apreceu_mais}' ela apareceu {qtd_q_mais_aparece}x.")
