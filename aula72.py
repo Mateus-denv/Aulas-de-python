a1a1b76de04598cd7f2dc7f888e5605e88b127cb
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-14 09:00:42 -0300

    Laço de repetição FOR
    Criando: Laço de repetição FOR
    Conteudo usado:  Introdução ao for / in - estrutura de repetição para coisas finitas

---
+++ aula72.py
@@ -0,0 +1,18 @@
+# Laço de repetição FOR
+texto = "oi"
+i = 0
+tamanho_texto = len(texto)
+
+"""
+# WHILE é utilizado qundo nos *não* sabemos quantos laços de repetiçoes seram utilados
+
+    while i < tamanho_texto:
+    print(texto[i],i)
+    i += 1
+"""
+# FOR é utilizado qundo nos sabemos quantos laços de repetiçoes seram utilados
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'-{letra}'
+    print(letra)
+print(novo_texto)
