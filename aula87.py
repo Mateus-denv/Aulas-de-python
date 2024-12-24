be18f56c11e7b74166fcfca3afdcd80e100e6805
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-24 17:28:47 -0300

    Corrigido

--- aula87.py
+++ aula87.py
@@ -1,8 +1,11 @@
 """ 
-Introdução ao desampacotamento + tuples (tuplas)
+Introdução ao desampacotamento 
 """
 nome1, nome2, nome3 =['Maria','Helena','Luiz']
 print(nome2)
 #Para pegar o resto
 nome1, *resto =['Maria','Helena','Luiz'] # Use o *_ enves  de *resto
 print(resto)
+#Para guarda o segudo valor ou terceiro assim por endiante
+_,*resto2, _, nome4 =['Maria','Helena','Luiz','Pedro']
+print(resto2)
