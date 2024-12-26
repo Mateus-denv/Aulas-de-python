5c9397f0df042ecd56cae9196b155caa0bb6229a
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-26 10:44:40 -0300

    aula 97
    Criando: Operação ternaria
    Conteudo usado: Condições em print

---
+++ aula97.py
@@ -0,0 +1,7 @@
+""" 
+Operação ternaria (Condicional de uma linha)
+<valor> if <condicão> else <outro valor >"""
+
+condicao = 1 == 2
+vareavel = 'valor' if condicao else "outro valor"
+print(vareavel)
