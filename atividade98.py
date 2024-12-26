908ca3adbcafd49c3c365dc24eb949422f2c99a9
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-26 10:05:02 -0300

    Feito a verificação do CPF
    Criando:  Validador de CPF
    Conteudo usado: Todos os anteriores a esse commit

---
+++ atividade98.py
@@ -0,0 +1,22 @@
+i = 0
+while i == 0:
+    print("Bom dia!! \nDigite o seu CPF abaixo")
+    CPF_cru = input("SEM OS '.' E '-' |> ")
+    digitos = 0
+    
+    # capturo os a quantidade de digitos e os numeros que foram digitados invalidos caso aja um cacter exibo a tela.
+    for confirmar in CPF_cru:
+        try:
+            digitos += 1
+        except ValueError:
+            print("Por favor, utilize somente numeros")
+            break
+
+    # Verico se á 11 digitos 
+    print(f"{digitos=}")        
+    if digitos < 11: 
+        print("Digite novamente os 11 digitos")
+        continue
+    else:
+        print("Digitos validos")
+    
