7e17092f8b63f8378aba2018c8dd039bb4752f5e
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2024-12-29 16:06:09 -0300

    Etapa 2.6
    Tentando criar a validação

--- ATV.validação_cpf.py
+++ ATV.validação_cpf.py
@@ -17,8 +17,7 @@
     2.2 Coleta a soma dos resultados dos 10 primeiros digitos e soma-los.
     2.3 Multiplicar a soma desses valores por 10.
     2.4 Obter o resto da multipicação por % 11.
-    2.5 Foram ciaridas 2 lista, a 1º para armazena o valor que o usuario enviou, e a 2º para armazena o valor que o programa gerou.
-    2.6 Se o resto for maior que 9 o penúltimo numero devera se torma 0, caso contrario permance o valor do resto.
+    2.5 Se o resto for maior que 9 o penúltimo numero devera se torma 0, caso contrario permance o valor do resto.
 """
 # ETAPA 1.
 while True:
@@ -50,7 +49,7 @@ while True:
     # 1.5
     numeros_multip_por_10 = soma_dos_nove_primerio_digitos * 10
     # 1.6
-    resto_multip_por_10 = numeros_multip_por_10 % 11
+    resto_multip_por_10_1 = numeros_multip_por_10 % 11
     
     # 1.7
     cpf_gerado_por_programa = []
@@ -59,15 +58,15 @@ while True:
         cpf_gerado_por_programa.append(digito)
         cpf_enviador_por_user.append(digito)
     # 1.8
-    if resto_multip_por_10 > 9:
-        print(f"Este digito [{resto_multip_por_10}] não é valido e será substituido para '0'.")
+    if resto_multip_por_10_1 > 9:
+        print(f"\nEste digito [{resto_multip_por_10_1}] não é valido e será substituido para '0'.")
         cpf_gerado_por_programa.pop(9)
-        cpf_gerado_por_programa.insert(9, 0)
+        cpf_gerado_por_programa.insert(9, "0")
     else:
-        print(f"Este digito [{resto_multip_por_10}] é valido.")
+        print(f"\nEste digito [{resto_multip_por_10_1}] é valido.")
         cpf_gerado_por_programa.pop(9)
-        cpf_gerado_por_programa.insert(9, resto_multip_por_10)
-    print("Enviado por você: ",*cpf_enviador_por_user, sep="")
+        cpf_gerado_por_programa.insert(9, str(resto_multip_por_10_1))
+    print("\nEnviado por você: ",*cpf_enviador_por_user, sep="")
     print("Gerado pelo programa: ",*cpf_gerado_por_programa,sep= "")
     
     # 2.1 
@@ -80,5 +79,24 @@ while True:
         multiplicador -=1
     # 2.3
     numeros_multip_por_11 = soma_dos_dez_primeiro_digitos * 10
-    print(numeros_multip_por_11)
+    # 2.4
+    resto_multip_por_10_2 = numeros_multip_por_11 % 11
+    # 2.5
+    if resto_multip_por_10_2 > 9:
+        print(f"\nEste digito [{resto_multip_por_10_2}] não é valido e será substituido para '0'.")
+        cpf_gerado_por_programa.pop(10)
+        cpf_gerado_por_programa.insert(10, "0")
+    else:
+        print(f"\nEste digito [{resto_multip_por_10_2}] é valido.")
+        cpf_gerado_por_programa.pop(10)
+        cpf_gerado_por_programa.insert(10, str(resto_multip_por_10_2))
+    print("\nEnviado por você: ",*cpf_enviador_por_user, sep="")
+    print("Gerado pelo programa: ",*cpf_gerado_por_programa,sep= "")
+    
+    
+    for i, digito in enumerate(cpf_gerado_por_programa):
+        if cpf_enviador_por_user[i] == cpf_gerado_por_programa[i]:
+            if i == 11:  
+                print("Seu CPF É VALIDO")
+        
     break
