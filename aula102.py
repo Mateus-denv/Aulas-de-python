77796aacf2c655987135c625a4981482cabc33a9
Mateus De Jesus Santos <mateussant0sc0osta@outlook.com>
2025-01-01 20:25:38 -0300

    Update aula102.py

--- aula102.py
+++ aula102.py
@@ -3,3 +3,9 @@
 # Erro 1: O uso de outros caracteres pelo usuario
 # Solução 1
 """cpf_enviador_por_user = input("Digite o seu CPF abaixo \nSOMENTE NUMEROS\n|>").replace("-","")"""
+# Solução 2
+import re
+entrada = input("Digite o seu CPF abaixo \nSOMENTE NUMEROS\n|>")
+cpf_enviador_por_user = re.sub(r'[^0-9]','',entrada)
+
+print(cpf_enviador_por_user)
